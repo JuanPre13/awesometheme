0782dd7f787cdd575714a6a04a02c9338f13ab37
juanpredev <prendas52@gmail.com>
2019-05-12 23:49:57 -0600

    Entendiendo como usar loops

--- header.php
+++ header.php
@@ -10,6 +10,17 @@
     ?>
     
 </head>
-<body>
+<?php
+      //{is_home()} = Detects if the query for the blog homepage
+     //{is_front_page()} = Determines whether the query is for the front page of the site.
+     
+    if():
+        $awesome_class = array('awesome-class','my-class');
+    else:
+        $awesome_class = array('no-class');  
+    endif;
+?>
+          <!--call to dynamic classes -->
+<body <?php body_class($awesome_class);?>>
     <?php wp_nav_menu(array('theme_location'=>'primary')); ?>
     
--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php get_header() ?>
-    <h1>This is my index</h1>
-<?php get_footer() ?>
+    <?php
+        //checks to see if the current WordPress query has any results to loop over.
+        if(have_posts()):
+
+            while(have_posts()):the_post();?>
+            
+                <h2><?php the_title();?></h3>
+                <small>Posted on: <?php the_time('F j, Y'); ?>, in <?php the_category() ?></small>
+                <p><?php the_content(); ?></p>
+                <hr>
+            <?php endwhile;
+        else:
+            echo("Don't have posts, sorry");
+
+        endif;
+    ?>
+
+
+<?php get_footer(); ?>
